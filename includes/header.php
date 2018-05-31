04096945a4bc872bc982ecdd78005fbc782bef52
David Mendoza <=>
2018-05-31 17:31:27 +0800

    created header
    Signed-off-by: David Mendoza <=>

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="ie" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US"><!--<![endif]-->
+<head>
+	<!-- Basic Page Needs -->
+	<meta charset="UTF-8">
+	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+	<title>FSB Consultants Inc.</title>
--- index-v2.php
+++ index-v2.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<!--[if IE 8 ]><html class="ie" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!-->
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US"><!--<![endif]-->
-<head>
-    <!-- Basic Page Needs -->
-    <meta charset="UTF-8">
-    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
-    <title>Gravita | Responsive Multi-Purpose HTML Template</title>
+    <?php include('includes/header.php') ?>
 
     <meta name="author" content="themsflat.com">
 
